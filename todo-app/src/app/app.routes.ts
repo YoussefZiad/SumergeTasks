f39895e51ee27e61af38e91a441710842c1cf2e7
Youssef Ziad <yzfm2000@gmail.com>
2025-06-03 03:41:16 +0300

    Added multi-step form (survey)

--- todo-app/src/app/app.routes.ts
+++ todo-app/src/app/app.routes.ts
@@ -1,16 +1,21 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { authGuard } from './app.guards';
+import { SurveyComponent } from './survey/survey.component';
 // import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {
+        path: '',
+        component: AuthComponent
+    },
+    {
         path: 'todo',
         loadComponent: () => import('./todo-list/todo-list.component').then(mod => mod.TodoListComponent),
         canActivate: [authGuard]
-    },
+    },  
     {
-        path: '',
-        component: AuthComponent
+        path: 'survey',
+        component: SurveyComponent
     }
 ];
