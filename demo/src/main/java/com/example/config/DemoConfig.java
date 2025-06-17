1037aec802127eee469d21025105312699c380ee
Youssef Ziad <yzfm2000@gmail.com>
2025-06-18 01:52:40 +0300

    Added JDBC Template CRUD Service/Repo

--- demo/src/main/java/com/example/config/DemoConfig.java
+++ demo/src/main/java/com/example/config/DemoConfig.java
@@ -1,6 +1,7 @@
 package com.example.config;
 
 import com.example.demo.CourseRecommenderImpl2;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.CourseRecommenderImpl3Override;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,11 +9,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import com.examplelib.config2.ExternalConfig;
 import com.examplelib.external.CourseRecommender;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.example.demo")
 @Import(ExternalConfig.class)
 public class DemoConfig {
+
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired  // Optional, Spring will autowire DataSource anyway
+    public DemoConfig(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     @Bean("SecondaryRecommender")
     public CourseRecommender secondaryCourseRecommender(){
---
+++ demo/src/main/java/com/example/course/CourseService.java
@@ -0,0 +1,37 @@
+package com.example.course;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    private JDBCCourseRepository courseRepository;
+
+    @Autowired
+    public CourseService(JDBCCourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public void addCourse(String name, String description, int credit){
+        courseRepository.addCourse(name, description, credit);
+    }
+
+    public void updateCourse(int id, String name, String description, Integer credit){
+        courseRepository.updateCourse(id, name, description, credit);
+    }
+
+    public void deleteCourse(int id){
+        courseRepository.deleteCourse(id);
+    }
+
+    public Course viewCourse(int id){
+        return courseRepository.viewCourse(id);
+    }
+
+    public List<Course> viewCourses(){
+        return courseRepository.viewCourses();
+    }
+}
