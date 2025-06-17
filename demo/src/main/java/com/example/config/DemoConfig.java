0ff888270608c5d264bdcc4f4a61abd29e97b4d0
Youssef Ziad <yzfm2000@gmail.com>
2025-06-18 01:52:40 +0300

    Added JDBC Template CRUD Service/Repo

--- demo/src/main/java/com/example/config/DemoConfig.java
+++ demo/src/main/java/com/example/config/DemoConfig.java
@@ -1,24 +1,21 @@
 package com.example.config;
 
-import com.example.demo.CourseRecommender;
-import com.example.demo.CourseRecommenderImpl2;
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import com.example.config.ExternalConfig;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
-@ComponentScan("com.example.demo")
-@Import(ExternalConfig.class)
+@ComponentScan("com.example")
 public class DemoConfig {
 
-    @Bean("SecondaryRecommender")
-    public CourseRecommender secondaryCourseRecommender(){
-        return new CourseRecommenderImpl2();
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired  // Optional, Spring will autowire DataSource anyway
+    public DemoConfig(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
 }
--- demo/src/main/java/com/example/demo/CourseRecommender.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import java.util.List;
-
-public interface CourseRecommender {
-
-    List<Course> recommendCourses();
-
-}
