517a3984062ac8086808238a18896fb8e9466a28
Youssef Ziad <yzfm2000@gmail.com>
2025-06-23 14:20:05 +0300

    Fixed corrupted rebase

--- demo/src/main/java/com/example/config/DemoConfig.java
+++ demo/src/main/java/com/example/config/DemoConfig.java
@@ -1,6 +1,7 @@
 package com.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,5 +18,6 @@ public class DemoConfig {
     public DemoConfig(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+
 
 }
